60c8791b9626e83862edcc600b18b670c1f9b621
Eric Espinoza <espinoza.eric@gmail.com>
2024-12-08 19:55:36 -0300

    add: updateMovie

--- src/main/java/com/avanzado/movies_backend/controllers/MovieController.java
+++ src/main/java/com/avanzado/movies_backend/controllers/MovieController.java
@@ -3,6 +3,7 @@ package com.avanzado.movies_backend.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -62,5 +64,20 @@ public class MovieController {
         return ResponseEntity.noContent().build();
 
     } 
+
+    @PutMapping("/detail/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody  Movie updatedMovie) {
+        
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        // agregamos el id en el movie
+        updatedMovie.setId(id);
+        
+        Movie savedMovie =  movieRepository.save(updatedMovie);
+
+        return ResponseEntity.ok(savedMovie);
+    }
 
 }
