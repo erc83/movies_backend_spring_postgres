e74c23b3774deb71f24bf944f579cfff9e0eff58
Eric Espinoza <espinoza.eric@gmail.com>
2024-12-08 18:05:39 -0300

    add: delete movie by id

--- src/main/java/com/avanzado/movies_backend/controllers/MovieController.java
+++ src/main/java/com/avanzado/movies_backend/controllers/MovieController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -48,5 +49,18 @@ public class MovieController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body( savedMovie );
     }
+
+    @CrossOrigin
+    @DeleteMapping("/detail/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id ) {
+        // si no existe
+        if(!movieRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+        movieRepository.deleteById(id);
+
+        return ResponseEntity.noContent().build();
+
+    } 
 
 }
